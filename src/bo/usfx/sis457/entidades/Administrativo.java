0d9e2798d6f050727c3ab0469571afe566d1e5a3
jhamil gonzales ramirez <jhamilex.jgr@gmail.com>
2018-05-13 23:19:28 -0400

    Nueva clase Administrativo que hereda de la clase Persona

--- src/bo/usfx/sis457/entidades/Administrativo.java
+++ src/bo/usfx/sis457/entidades/Administrativo.java
@@ -5,10 +5,65 @@
  */
 package bo.usfx.sis457.entidades;
 
+import java.util.Calendar;
+
 /**
  *
- * @author Marcelo
+ * @author Jhamil
  */
-public class Administrativo {
+public class Administrativo extends Persona{
+    protected String Cargo;
+    protected String LugarTrabajo;
+    protected Calendar FechaIngreso;
+
+    public Administrativo(String Cargo, String LugarTrabajo, Calendar FechaIngreso, String carnetIdentidad, String nombre, Calendar fechaNacimiento) {
+        super(carnetIdentidad, nombre, fechaNacimiento);
+        this.Cargo = Cargo;
+        this.LugarTrabajo = LugarTrabajo;
+        this.FechaIngreso = FechaIngreso;
+    }
+
+    public Administrativo(String Cargo, String LugarTrabajo, Calendar FechaIngreso) {
+        this.Cargo = Cargo;
+        this.LugarTrabajo = LugarTrabajo;
+        this.FechaIngreso = FechaIngreso;
+    }
+    public Administrativo() {
+        
+    }
+    public String getCargo() {
+        return Cargo;
+    }
+
+    public void setCargo(String Cargo) {
+        this.Cargo = Cargo;
+    }
+
+    public String getLugarTrabajo() {
+        return LugarTrabajo;
+    }
+
+    public void setLugarTrabajo(String LugarTrabajo) {
+        this.LugarTrabajo = LugarTrabajo;
+    }
+
+    public Calendar getFechaIngreso() {
+        return FechaIngreso;
+    }
+
+    public void setFechaIngreso(Calendar FechaIngreso) {
+        this.FechaIngreso = FechaIngreso;
+    }
     
+    @Override
+    public String toString(){
+        return "Administrativo{"
+                + ", Id=" +this.Id
+                + ", CarnetIdentidad=" + this.CarnetIdentidad
+                + ", Nombre=" +this.Nombre
+                + ", FechaNacimiento=" + this.FechaNacimiento
+                + ", Cargo=" + this.Cargo
+                + ", LugarTrabajo=" + this.LugarTrabajo
+                + ", FechaIngreso=" + this.FechaIngreso+'}';
+    }
 }
